90f784cddd59d2148e9890cf887630a5564d320e
wjosephmark <wjosephmark@gmail.com>
2020-08-15 12:00:32 -0600

    put text inside of gradient

--- src/components/app.js
+++ src/components/app.js
@@ -4,20 +4,20 @@ export default function Component() {
 
   return (
     <div className='app'>
+
       <div className="gradient-wrapper">
         <div className="gradient">
+          <div className="text-container">
+            <div className="introduction-wrapper">
+              <h1>Hello, my name is Heidi Mark</h1>
+            </div>
+            <div className="mission-wrapper">
+              <h1>I coach women on intentional self care.  It’s not an indulgence, it’s a necessity.</h1>
+            </div>
+          </div>
         </div>
-
-
       </div>
-        <div className="text-container">
-          <div className="introduction-wrapper">
-            <h1>Hello, my name is Heidi Mark</h1>
-          </div>
-          <div className="mission-wrapper">
-            <h1>Mission Statement</h1>
-          </div>
-        </div>
+
     </div>
 
   );
